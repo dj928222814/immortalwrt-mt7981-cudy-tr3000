895bf8c231cbd90ebcf5c18fa062c3a7cf677393
dj928222814 <90567213+dj928222814@users.noreply.github.com>
2026-06-30 22:32:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,3 +27,5 @@ git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+# 添加 QModem 软件源
+echo 'src-git qmodem https://github.com' >> feeds.conf.default
