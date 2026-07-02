e46d0e05e96878e983151f1b6d18d05820b2c6c5
dj928222814 <90567213+dj928222814@users.noreply.github.com>
2026-07-03 02:27:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,39 +4,17 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 强行删除系统 Feeds 软件源里自带的旧版 Argon 主题，防止与你刚刚克隆的最新版源码发生冲突
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# 临时解决Rust问题
+# 2. 临时解决 Rust 编译器的编译问题
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
-# add date in output file name
+# 3. 在输出的固件文件名中自动加上编译日期
 sed -i -e '/^IMG_PREFIX:=/i BUILD_DATE := $(shell date +%Y%m%d)' \
        -e '/^IMG_PREFIX:=/ s/\($(SUBTARGET)\)/\1-$(BUILD_DATE)/' include/image.mk
 
-# set ubi to 122M
-# sed -i 's/reg = <0x5c0000 0x7000000>;/reg = <0x5c0000 0x7a40000>;/' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1-ubootmod.dts
-# 自动在配置文件中勾选 QModem 及其 Luci 界面
-echo "CONFIG_PACKAGE_luci-app-qmodem=y" >> .config
-echo "CONFIG_PACKAGE_qmodem=y" >> .config
-# 自动在配置文件中勾选 iStore 及其 Luci 界面
-echo "CONFIG_PACKAGE_luci-app-store=y" >> .config
-echo "CONFIG_PACKAGE_istore=y" >> .config
-# 强制将默认主题修改为 luci-theme-argon
+# 4. 强制将系统的默认主题修改为高颜值的 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 自动在配置文件中勾选 Argon 主题及其配置后台
-echo "CONFIG_PACKAGE_luci-theme-argon=y" >> .config
-echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config
